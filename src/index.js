9f1d743683d8a1130ad7bc847547523ea17c73a0
Jahlom Precious Agboado <jahlomp@gmail.com>
2016-02-23 00:15:32 +0000

    Adding a function for getting country codes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
+var objectKeys = require('object-keys');
 var objectValues = require('object-values');
 var countryNames = require('./countries.json');
 
 module.exports = {
-    all: objectValues(countryNames)
+    all: objectValues(countryNames),
+    getCountryCodes: objectKeys(countryNames)
 };

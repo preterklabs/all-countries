542156f0f7a71a2e435a4263815e9194a3473283
Jahlom Precious Agboado <jahlomp@gmail.com>
2016-02-23 00:52:23 +0000

    Adding a function for getting a country based on an assigned code

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 var objectKeys = require('object-keys');
 var objectValues = require('object-values');
-var countryNames = require('./countries.json');
+var countries = require('./countries.json');
 
 module.exports = {
-    all: objectValues(countryNames),
-    getCountryCodes: objectKeys(countryNames)
+    all: objectValues(countries),
+    getCountryCodes: objectKeys(countries),
+    getCountryByCode: function (code) {
+        if (code && typeof(code) === 'string') {
+            return countries[code];
+        }
+    }
 };

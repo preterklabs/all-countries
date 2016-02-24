cdef86f492b571be054ff93c3f3c201c30d84784
Jahlom Precious Agboado <jahlomp@gmail.com>
2016-02-24 13:12:31 +0000

    added country phone code

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ var objectValues = require('./utils/object-values');
 var countries = require('./data/countries.json');
 var capitals = require('./data/capital.json');
 var currencies = require('./data/currency.json');
+var phone = require('./data/phone.json');
 
 module.exports = {
     all: objectValues(countries),
@@ -12,16 +13,37 @@ module.exports = {
             return countries[code];
         }
     },
+    // TODO
+    // getCountryByCapital: function (capital) {
+    //     // Object.keys(capitals)[0]
+    // }
+    // // TODO
+    // getCountryByCuurency: function (capital) {
+    //     // Object.keys(capitals)[0]
+    // }
+    // // TODO
+    // getCountryByPhoneCode: function (capital) {
+    //     // Object.keys(capitals)[0]
+    // }
     getCapitals: objectValues(capitals),
+    // TODO Add the ability to accept country name too
     getCapitalByCountryCode: function (code) {
         if (code && typeof code === 'string') {
             return capitals[code];
         }
     },
     getCurrencies: objectValues(currencies),
+    // TODO Add the ability to accept country name too
     getCurrencyByCountryCode: function (code) {
         if (code && typeof code === 'string') {
             return currencies[code];
+        }
+    },
+    getPhoneCodes: objectValues(phone),
+    // TODO Add the ability to accept country name too
+    getPhoneCodeByCountryCode: function (code) {
+        if (code && typeof code === 'string') {
+            return phone[code];
         }
     }
 };

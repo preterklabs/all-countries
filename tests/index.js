1a544711732d695fd53a306eb3c85d4eed765ff0
Jahlom Precious Agboado <jahlomp@gmail.com>
2016-02-26 13:35:17 +0000

    added for more tests

--- tests/index.js
+++ tests/index.js
@@ -13,4 +13,52 @@ describe('all-countries', function () {
             }
         });
     });
+
+    describe('getCountryCodes', function () {
+        it('should be an array of strings', function (){
+            expect(allCountries.getCountryCodes).to.satisfy(isArrayOfStrings);
+
+            function isArrayOfStrings(array) {
+                return array.every(function(item) {
+                    return typeof item === 'string';
+                })
+            }
+        });
+    });
+
+    describe('getCapitals', function () {
+        it('should be an array of strings', function (){
+            expect(allCountries.getCapitals).to.satisfy(isArrayOfStrings);
+
+            function isArrayOfStrings(array) {
+                return array.every(function(item) {
+                    return typeof item === 'string';
+                })
+            }
+        });
+    });
+
+    describe('getCurrencies', function () {
+        it('should be an array of strings', function (){
+            expect(allCountries.getCurrencies).to.satisfy(isArrayOfStrings);
+
+            function isArrayOfStrings(array) {
+                return array.every(function(item) {
+                    return typeof item === 'string';
+                })
+            }
+        });
+    });
+
+    describe('getPhoneCodes', function () {
+        it('should be an array of strings', function (){
+            expect(allCountries.getPhoneCodes).to.satisfy(isArrayOfStrings);
+
+            function isArrayOfStrings(array) {
+                return array.every(function(item) {
+                    return typeof item === 'string';
+                })
+            }
+        });
+    });
 });
